c57a1e187c0b1cbfc08dd1a91fd61d415ba6374c
HexPlex <sendhilsom7@gmail.com>
2020-09-07 22:08:13 +0530

    setup

---
+++ gym_KiloBot/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='gym_kiloBot',
+    version='0.0.1',
+    description='An OpenAI Gym env for a swarm of kilo bots which have a limited perception of the world',
+    packages=setuptools.find_package(include="gym_kiloBot"),
+    intall_required=['gym','pygame','opencv-python'],
+    package_data={'':[]} 
+
+)
