07d76918e44a802e9f866e6d7e970f08b3150e49
HexPlex <sendhilsom7@gmail.com>
2020-09-08 12:10:58 +0530

    setup

---
+++ gym-KiloBot/gym_KiloBot/envs/__init__.py
@@ -0,0 +1,1 @@
+from gym_Kilobot.envs.kilobot_env import KilobotEnv
---
+++ gym-KiloBot/gym_KiloBot/envs/kilobot_env.py
@@ -0,0 +1,20 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+import pygame
+import numpy
+import cv2
+class KiloBotEnv(gym.Env):
+    metadata={'render.modes':['human']}
+
+    def __init__(self):
+        pass
+
+    def step(self,action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self,mode='human',close=False):
+        pass
