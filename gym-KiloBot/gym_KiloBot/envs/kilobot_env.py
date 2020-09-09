acb7fb6f80bf78d37f25dc961753c7c3459f5f03
HexPlex <sendhilsom7@gmail.com>
2020-09-09 15:48:37 +0530

    Objective defined

--- gym-KiloBot/gym_KiloBot/envs/kilobot_env.py
+++ gym-KiloBot/gym_KiloBot/envs/kilobot_env.py
@@ -11,14 +11,20 @@ class KiloBotEnv(gym.Env):
     metadata={'render.modes':['human']}
     BLACK=(0,0,0);WHITE=(255,255,255)
     pygame.init()
-    def __init__(self,n=5,module_color=(0,255,0),radius=5,screen_width=250,screen_heigth=250):
+    def __init__(self,n=5,objective="graph",module_color=(0,255,0),radius=5,screen_width=250,screen_heigth=250):
         super().__init__()     ##Check it  once never used before
         self.modules = []
+        if objective=="localization":
+            self.obj = True
+        else:
+            self.obj = False
         self.module_color = module_color
         self.screen_width = screen_width
         self.screen_heigth = screen_heigth
         self.screen = pygame.display.set_mode((self.screen_width,self.screen_heigth))
         pygame.display.set_caption("Swarm")
+        self.screen.fill(self.BLACK)
+        self.radius = radius
         for i in range(n):
             self.modules.append(KiloBot(module_color,
                                     radius,
@@ -29,15 +35,39 @@ class KiloBotEnv(gym.Env):
                                     screen_heigth=self.screen_heigth)
                                     )
 
+    def fetch_histogram(self,states):
+        pass
 
+    def graph_obj_distances(self):
+        pass
 
     def step(self,actions):
-        for module,action in zip(modules,actions):
-            module.update(action)
+        states=[]
+        reward = 0
+        self.screen.fill(self.BLACK)
+        for module,action in zip(self.modules,actions):
+            reward -= 0.05 * module.update(action)
+            states.append(module.get_state())
+            pygame.draw.rect(self.screen,module.color,module.rect)
+            ## Draw a arrow for the same
+            ## Draw A circle around it and draw the Region of interest
+        if self.obj:
+            pygame.draw.circle(self.screen,self.BLUE,self.target) ## draw  the blue dot
+        else:
+            pass ## Draw the relationship joints also
+        done = False
+        info={}
+        critic_input = np.array(pygame.PixelArray(self.screen),dtype=np.uint8).T.reshape([self.screen_width,self.screen_heigth,1])
+        observation = [states,critic_input]
+        return states,reward,done,info
+
 
     def reset(self):
-
+        self.screen.fill(self.BLACK)
         for module in modules:
             module.spawn()
+        if self.obj:
+            self.target = (np.random.randint(self.radius,self.screen_width-self.radius),np.random.randint(self.radius,self.screen_heigth-self.radius))
+            pygame.draw.circle(self.screen,self.BLUE,self.target)
     def render(self,mode='human',close=False):
         pass
