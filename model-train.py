e3b5d9ac69bdc3c96d6a4a1e1382898748db49a0
HexPlex <sendhilsom7@gmail.com>
2020-09-15 00:52:43 +0530

    Online Training Done

--- model-train.py
+++ model-train.py
@@ -16,7 +16,7 @@ import time
 FLAGS = flags.FLAGS
 flags.DEFINE_boolean("headless",False,"False to render the environment")
 flags.DEFINE_integer("modules",10,"Defines the no of modules in the env")
-flags.DEFINE_integer("time_steps",10000,"This is the no of steps that the env would take before stoping")
+flags.DEFINE_integer("time_steps",10000000,"This is the no of steps that the env would take before stoping")
 flags.DEFINE_integer("histRange",10,"Defines the steps for the histograms")
 flags.DEFINE_string("objective","graph","This defines which task is to be choosen")
 flags.DEFINE_string("logdir","logs","Defines the logging directory")
@@ -44,22 +44,16 @@ class CustomCallBack(tf.keras.callbacks.Callback):
         item_to_write={
             'loss':logs.get('loss')
             }
-        for name, value in item_to_write.items():
-            summary = tf.summary.Summary()
-            summary_value = summary.value.add()
-            summary_value.simple_value = value
-            summary_value.tag = self.tag+name
-            self.writer.add_summary(summary,self.step_number)
-            self.writer.flush()
+        with self.writer.as_default():
+            for name, value in item_to_write.items():
+                tf.summary.scalar(self.tag+name,value,step=self.step_number)
+
     def inter_post(self,name,value,n=None):
-        summary = tf.summary.Summary()
-        summary_value = summary.value.add()
-        summary_value.simple_value = value
-        summary_value.tag = name
         if n is None:
             n = self.step_number
-        self.writer.add_summary(summary,n)
-        self.writer.flush()
+        with self.writer.as_default():
+            tf.summary.scalar(name,value,step=n)
+
     def step_one(self):
         self.step_number +=1
     def __call__(self,tag,info={}):
@@ -72,16 +66,16 @@ class ModelCritic(tf.keras.Model):
     def __init__(self,input_dims):
         super().__init__()
         self.state_input=Input(shape=input_dims,name="state_input")
-        self.f1 = Dense(512,activation='elu',name='forward1',kernel_initializer=keras.initializers.RandomUniform(minval=-1./512,maxval=1./512))
-        self.f2 = Dense(256,activation='elu',name='forward2',kernel_initializer=keras.initializers.RandomUniform(minval=-1./256,maxval=1./256))
-        self.f3 = Dense(128,activation='elu',name='forward3',kernel_initializer=keras.initializers.RandomUniform(minval=-1./128,maxval=1./128))
+        self.fc1 = Dense(512,activation='elu',name='forward1',kernel_initializer=keras.initializers.RandomUniform(minval=-1./512,maxval=1./512))
+        self.fc2 = Dense(256,activation='elu',name='forward2',kernel_initializer=keras.initializers.RandomUniform(minval=-1./256,maxval=1./256))
+        self.fc3 = Dense(128,activation='elu',name='forward3',kernel_initializer=keras.initializers.RandomUniform(minval=-1./128,maxval=1./128))
         self.value_func = Dense(1,activation='linear',name='value_func',kernel_initializer=keras.initializers.RandomUniform(minval=-3e-4,maxval=3e-4))
 
     def call(self,input_data):
-        x = self.state_input(input_data)
-        x = self.f1(x)
-        x = self.f2(x)
-        x = self.f3(x)
+        #x = self.state_input(input_data)
+        x = self.fc1(input_data)
+        x = self.fc2(x)
+        x = self.fc3(x)
         v = self.value_func(x)
         return v
 
@@ -111,10 +105,10 @@ class ModelActor(tf.keras.Model):
         self.mu = Dense(no_action,activation='linear',name='mu1',kernel_initializer=keras.initializers.RandomUniform(minval=-3e-3,maxval=3e-3))
         self.sigma = Dense(no_action,activation='linear',name='sigma1',kernel_initializer=keras.initializers.RandomUniform(minval=-3e-3,maxval=3e-3))
     def call(self,input_data):
-        x = self.state_input(input_data)
-        x = self.f1(x)
-        x = self.f2(x)
-        x = self.f3(x)
+        #x = self.state_input(input_data)
+        x = self.fc1(input_data)
+        x = self.fc2(x)
+        x = self.fc3(x)
         x = self.fc4(x)
         probmu = self.mu(x)
         probsigma = self.sigma(x)
@@ -126,18 +120,19 @@ class ModelActor(tf.keras.Model):
         self.optimizer = Adam(lr=hyperparam['actor_lr'])
 
     def act(self,state):
-        probmu, probsigma = self(np.array[state])
+        probmu, probsigma = self(np.array(state))
         dist = tfp.distributions.Normal(loc=probmu.numpy(),scale=probsigma.numpy())
         action = dist.sample([1])
         return action.numpy()
     def actor_loss(self,probmu,probsigma,actions,td):
-        dist = tfp.distributions.Normal(loc=probmu.numpy(),scale=probsigma.numpy())
+        dist = tfp.distributions.Normal(loc=probmu,scale=probsigma)
         log_prob = dist.log_prob(actions + 1e-5)
         loss = -log_prob*td
         return loss
-    def learn(self,prev_state,action,td):
+    def learn(self,prev_state,td):
         with tf.GradientTape() as tape:
             pm,ps = self(prev_state,training=True)
+            action = self.act(prev_state)
             a_loss = self.actor_loss(pm,ps,action,td)
         grads = tape.gradient(a_loss,self.trainable_variables)
         self.optimizer.apply_gradients(zip(grads,self.trainable_variables))
@@ -155,19 +150,21 @@ def preprocessReplayBuffer(states,actions,rewards,gamma):
     discountedRewards = np.array(discountedRewards,dtype=np.float32)
 
     return states, actions, discountedRewards
-def fetch_states_localize(observation,info):
+def fetch_states_localize(observation,info,env):
     prev_state = np.append(np.array(info.get('localization_bit')).reshape(-1,1),
                             observation,axis=1)
     prev_state = np.append(np.array(info.get('target_distance')).reshape(-1,1),
                             prev_state,axis=1)
     prev_state = np.append(np.array(info.get('neighbouring_bit')).reshape(-1,1),
                             prev_state,axis=1)
-    critic_prev_state = np.array([module.get_state() in env.modules],dtype=np.float32).reshape(1,-1)
+    critic_prev_state = np.array([module.get_state() for module in env.modules],dtype=np.float32).reshape(1,-1)
     critic_prev_state = np.append(np.array(env.target).reshape(1,-1),critic_prev_state)
     return prev_state,critic_prev_state
 
-def fetch_states_graph(observation,info):
-    
+def fetch_states_graph(observation,info,env):
+    prev_state = observation
+    critic_prev_state = np.array([module.get_state() for module in env.modules],dtype=np.float32).reshape(1,-1)
+    return prev_state,critic_prev_state
 def main(argv):
     env = gym.make("kiloBot-v0",
                     n=FLAGS.modules,
@@ -194,23 +191,40 @@ def main(argv):
     a = env.dummy_action(0.1,5)
     observation,_,_,info = env.step([a]*env.n)
     if obj:
-        prev_state = np.append(np.array(info.get('localization_bit')).reshape(-1,1),
-                                observation,axis=1)
-        prev_state = np.append(np.array(info.get('target_distance')).reshape(-1,1),
-                                prev_state,axis=1)
-        prev_state = np.append(np.array(info.get('neighbouring_bit')).reshape(-1,1),
-                                prev_state,axis=1)
-        critic_prev_state = np.array([module.get_state() in env.modules],dtype=np.float32).reshape(1,-1)
-        critic_prev_state = np.append(np.array(env.target).reshape(1,-1),critic_prev_state)
+        fetch_states = fetch_states_localize
     else:
-        prev_state = np.array(observation)
-
+        fetch_states = fetch_states_graph
+    prev_state,critic_prev_state = fetch_states(observation,info,env)
+    critic_model.setup(gamma=hyperparam['gamma'])
+    actor_model.setup(gamma=hyperparam['gamma'])
     best_reward = -100000
     while iter<FLAGS.time_steps:
         iter +=1
-        if obj:
-            state =
-
+        if not FLAGS.headless:
+            env.render()
+        action_inputs = np.squeeze(actor_model.act(prev_state))
+        print(action_inputs)
+        actions = []
+        for action_input in action_inputs:
+            print(action_input)
+            actions.append(env.dummy_action(max(min(action_input[0],2*np.pi),-2*np.pi),max(min(action_input[1],10),0)))
+        observation,reward,done,info = env.step(actions)
+        state,critic_state = fetch_states(observation,info,env)
+        td,critic_error = critic_model.learn(reward,critic_prev_state,critic_state)
+        actor_loss = actor_model.learn(prev_state,td)
+        custom_callback.inter_post("actor_loss",actor_loss,n=iter)
+        custom_callback.inter_post("critic_loss",critic_loss,n=iter)
+        custom_callback.step_one()
+        prev_state,critic_prev_state = state,critic_state
+        best_reward = max(reward,best_reward)
+        if iter%10000==0:
+            actor_model.save_weights(savepath+"/actor_model.h5")
+            critic_model.save_weights(savepath+"/critic_model.h5")
+            env.reset()
+            observation,_,_,info = env.step([a]*env.n)
+            prev_state,critic = fetch_states(observation,info,env)
+        elif iter%100==0:
+            print("iter "+str(iter)+" yeilds reward :"+str(reward))
 
     env.close()
 
